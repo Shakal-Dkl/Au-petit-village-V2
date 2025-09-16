05118fd29425201cdea3ce860305dd0898d314e8
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2025-09-16 23:34:01 +0200

    hh

--- Au_petit_village/src/app/app.routes.ts
+++ Au_petit_village/src/app/app.routes.ts
@@ -1,17 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
 
-
-
-import { App } from './app';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
+import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { About } from './components/about/about';
 import { product} from './components/product/product';
-import { SortByPricePipe } from './pipes/sort-by-price-pipe';
-import { FilterByNamePipe } from './pipes/filter-by-name-pipe';
+
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -20,19 +12,5 @@ export const routes: Routes = [
   { path: '**', redirectTo: '' }
 ];
 
-@NgModule({
-   declarations: [
-    App,
-    Header,
-    Footer,
-    Home,
-    About,
-    product,
-    SortByPricePipe,
-    FilterByNamePipe
-  ],
-  imports: [RouterModule.forRoot(routes), BrowserModule,
-    AppRoutingModule],
-  exports: [RouterModule]
-})
+
 export class AppRoutingModule { }
