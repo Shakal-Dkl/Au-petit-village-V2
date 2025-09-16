138a6850b11daba4d383d1aa5b45b674e3d29852
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2025-09-16 18:47:14 +0200

    maj#10

--- Au_petit_village/src/app/pipes/filter-by-name-pipe.ts
+++ Au_petit_village/src/app/pipes/filter-by-name-pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../services/products';
 
 @Pipe({
   name: 'filterByName'
 })
 export class FilterByNamePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(products: Product[], searchTerm: string): Product[] {
+    if (!products || !searchTerm) {
+      return products;
+    }
+    
+    return products.filter(product => 
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   }
-
 }
