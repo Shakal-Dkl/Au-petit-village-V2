cf8c7814cd778bea9c34302fe6d066373971e490
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2025-09-19 09:11:22 +0200

    fg

--- Au_petit_village/src/app/components/product/product.ts
+++ Au_petit_village/src/app/components/product/product.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { ProductsService } from '../../services/products';
 
 interface Product {
   id: number;
